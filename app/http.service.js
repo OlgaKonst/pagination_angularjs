f5bb6b87f2a1af5b055aa2ea878ed0efa07db8fc
OlgaKonst <olhakonst@gmail.com>
2016-09-28 14:58:14 +0300

    add functions initPagination and changePage in main.controller.js

--- app/http.service.js
+++ app/http.service.js
@@ -1,4 +1,19 @@
 angular.module('myApp')
-  .service('usersService', function() {
+  .service('usersService', function($http) {
+    this.getUsers = function(link) {
+      return $http.get(link).then(function(response) {
+          console.log('ok');
+          //toastr.success(angular.toJson('data length: ' + response.data.length, true), ':)');
+          return response.data;
+        })
+        .catch(function(reason) {
+          console.error('не ok', reason);
+         // toastr.error(angular.toJson(reason.data, true), ':\'(');
+         // defer.reject(reason);
+        });
 
-  });
+   /*   catch(function(reason) {
+        console.error('not good', reason);
+});*/
+}
+});
--- app/main.controller.js
+++ app/main.controller.js
@@ -1,4 +1,39 @@
 angular.module('myApp')
-  .controller('MainController', ['$scope', function($scope) {
+  .controller('MainController', ['$scope', 'usersService', function($scope, usersService) {
+      $scope.count = 10;
+      $scope.currentPage = 0;
+      $scope.loading = true;
 
+    $scope.options = [4, 15, 30];
+
+      $scope.changePage = function(index) {
+          $scope.currentPage = index;
+          $scope.offset = $scope.count * index;
+      }
+      $scope.$watch('count', function(newValue) {
+          console.log('newValue',newValue);
+          if(!newValue) return;
+          if($scope.users) {
+              console.log('777');
+              initPagination($scope.users);
+          }
+      });
+
+    usersService.getUsers('dummy.json')
+        .then(initPagination)
+        .finally(function() {
+          $scope.loading = false;
+        });
+
+    function initPagination(data) {
+        console.log(222);
+      if(!$scope.users) {
+        $scope.users = data;
+       // console.log($scope.users);
+      }
+        $scope.totalPages = $scope.count ? Math.ceil($scope.users.length/$scope.count) : 1;
+        console.log('$scope.totalPages', $scope.totalPages);
+        $scope.pagination = new Array($scope.totalPages);
+        console.log($scope.pagination);
+    }
   }]);
